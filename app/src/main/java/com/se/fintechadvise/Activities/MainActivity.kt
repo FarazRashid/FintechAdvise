98c08a6d29e3abc690a3af9545d49762b38b1e4b
Faraz Rashid <muhammadfarazrashid@gmail.com>
2024-04-18 15:20:25 +0500

    Initial commit

---
+++ app/src/main/java/com/se/fintechadvise/Activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.se.fintechadvise.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.se.fintechadvise.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
